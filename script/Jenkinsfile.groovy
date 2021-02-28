643348d95cbd0134be930851a8340b163911da1b
Phien <phien@Macintoshs-MacBook-Pro.local>
2021-03-01 02:29:54 +0700

    Fixing

--- script/Jenkinsfile.groovy
+++ script/Jenkinsfile.groovy
@@ -85,7 +85,7 @@ pipeline {
 	    steps {
         dir('./') {
           sh """
-            helm upgrade --install phien-java-app --set image.repository=phienhoangnguyen/thesis-phien-2021 --set image.tag=1 --set image.pullPolicy=Always --set tomcatPassword=2MNxLHqfIg bitnami/tomcat
+            helm upgrade --install phien-java-app --set image.repository=phienhoangnguyen/thesis-phien-2021 --set image.tag=\${BUILD_NUMBER} --set image.pullPolicy=Always --set tomcatPassword=2MNxLHqfIg bitnami/tomcat
           """
         }
       }
